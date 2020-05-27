9bccb99b62d7691fe597eea071660b202e4fc258
samamahe <samamaheshreddy007@gmail.com>
2020-05-27 17:10:33 -0400

    Added permissions, login activity

--- app/src/main/java/com/example/roombookingsystem/activities/UserLoginActivity.java
+++ app/src/main/java/com/example/roombookingsystem/activities/UserLoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -15,40 +16,38 @@ import com.example.roombookingsystem.R;
 public class UserLoginActivity extends AppCompatActivity {
     Button btn_user_login;
     TextView tv_signup;
+    EditText mStaffID, mPassword;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userlogin);
 
-        getSupportActionBar().setTitle("User Login");
-        getSupportActionBar().setHomeButtonEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setTitle("User Login");
+//        getSupportActionBar().setHomeButtonEnabled(true);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        btn_user_login=(Button)findViewById(R.id.btn_user_login);
+        mStaffID = findViewById(R.id.et_user_id);
+        mStaffID = findViewById(R.id.et_password);
+
+        btn_user_login=(Button)findViewById(R.id.btn_login);
         tv_signup=(TextView)findViewById(R.id.tv_signup);
 
         tv_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(getApplicationContext(),RegistrationActivity.class);
+
                 startActivity(intent);
             }
         });
+
         btn_user_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
             }
         });
     }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                this.finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
 
-    }
+
 }
