f748d1ac268d5f15f26ebd81657b4961d77b6320
tagorekommineni9 <tagorek7@kluniversity.in>
2020-05-28 23:12:34 -0400

    Staff Registration Firebase with all details

--- app/src/main/java/com/example/roombookingsystem/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/roombookingsystem/activities/RegistrationActivity.java
@@ -1,31 +1,54 @@
 package com.example.roombookingsystem.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.roombookingsystem.R;
+import com.example.roombookingsystem.activities.staff.UserDashboardActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    EditText mStaffID, mName, mPhone, mEmail, mPassword, mConfirmPassword;
+    EditText mName, mPhone, mEmail, mPassword, mConfirmPassword;
     Button mRegister;
-    String staff_id, name, phone, email, password, confirm_pwd;
+    String  name, phone, email, password, confirm_pwd;
+    int staff_id;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activit_registration);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
         getSupportActionBar().setTitle("Registration");
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-        mStaffID = findViewById(R.id.et_staff_id);
         mName = findViewById(R.id.et_name);
         mPhone = findViewById(R.id.et_phone);
         mEmail = findViewById(R.id.et_email);
@@ -39,32 +62,68 @@ public class RegistrationActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if(TextUtils.isEmpty(mStaffID.getText().toString()))
-                {
-                    mStaffID.setError("Enter your ID");
-                }
-                else if(TextUtils.isEmpty(mName.getText().toString())){
+                if (TextUtils.isEmpty(mName.getText().toString())) {
                     mName.setError("Enter your Name");
-                }
-                else if(TextUtils.isEmpty(mPhone.getText().toString())){
+                } else if (TextUtils.isEmpty(mPhone.getText().toString())) {
                     mPhone.setError("Enter your Phone number");
-                }
-                else if(TextUtils.isEmpty(mEmail.getText().toString())){
+                } else if (TextUtils.isEmpty(mEmail.getText().toString())) {
                     mEmail.setError("Enter your Email");
-                }
-                else if(TextUtils.isEmpty(mPassword.getText().toString())){
+                } else if (TextUtils.isEmpty(mPassword.getText().toString())) {
                     mPassword.setError("Enter your Password");
-                }
-                else if(TextUtils.isEmpty(mConfirmPassword.getText().toString())){
+                } else if (TextUtils.isEmpty(mConfirmPassword.getText().toString())) {
                     mConfirmPassword.setError("Please confirm your password");
-                }
-                else {
+                } else if (!(mConfirmPassword.getText().toString().equals(mPassword.getText().toString()))){
+                    mConfirmPassword.setError("Passwords are not matching!");
+                } else {
+
+                    name = mName.getText().toString();
+                    email = mEmail.getText().toString();
+                    phone = mPhone.getText().toString();
+                    password = mPassword.getText().toString();
+
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+
+                                String staffId = firebaseAuth.getCurrentUser().getUid();
+                                DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("users").child(staffId);
+
+                                Map staffInfo = new HashMap<>();
+
+                                staffInfo.put("name", name);
+                                staffInfo.put("email", email);
+                                staffInfo.put("phone", phone);
+                                staffInfo.put("type", "staff");
+
+                                currentUserDb.updateChildren(staffInfo);
+                                Toast.makeText(RegistrationActivity.this, "Staff ID is created successfully! Please login!", Toast.LENGTH_SHORT).show();
+                                clearFields();
+
+                                Intent staffIntent = new Intent(RegistrationActivity.this, UserDashboardActivity.class);
+                                staffIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(staffIntent);
+
+
+
+                            }
+                        }
+                    });
 
 
                 }
             }
         });
-
-
     }
+
+    private void clearFields() {
+        mName.getText().clear();
+        mPassword.getText().clear();
+        mConfirmPassword.getText().clear();
+        mPhone.getText().clear();
+    }
+
+
+
 }
