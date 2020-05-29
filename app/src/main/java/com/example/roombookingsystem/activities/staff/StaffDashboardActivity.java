6c6989042e754f670be736e544d92e6a2bb90583
tagorekommineni9 <tagorek7@kluniversity.in>
2020-05-29 19:31:11 -0400

    Setup Staff dashboard with Navigation drawer layout with all fragments.

--- app/src/main/java/com/example/roombookingsystem/activities/staff/StaffDashboardActivity.java
+++ app/src/main/java/com/example/roombookingsystem/activities/staff/StaffDashboardActivity.java
@@ -1,16 +1,115 @@
 package com.example.roombookingsystem.activities.staff;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.roombookingsystem.R;
+import com.example.roombookingsystem.activities.UserLoginActivity;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class StaffDashboardActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
+    String firebaseCurrentUserID;
+    MaterialToolbar toolbar;
+    Boolean mSlideState=false;
+
+    public StaffDashboardActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_dashboard);
+        setContentView(R.layout.activity_staff_dashboard);
+
+        //Firebase Connections & id retrieval
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseCurrentUserID = firebaseAuth.getCurrentUser().getUid();
+
+        drawerLayout = findViewById(R.id.my_drawer);
+        navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle mDrawerToggle;
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_hamburger);
+        toolbar.setTitle("Staff Dashboard");
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!mSlideState){
+                drawerLayout.openDrawer(Gravity.LEFT);
+                mSlideState=true;
+                }
+                else {
+                    mSlideState=false;
+                    drawerLayout.closeDrawer(Gravity.LEFT);
+                }
+            }
+        });
+
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment = null;
+                switch (menuItem.getItemId()) {
+
+                    case R.id.nav_rooms_available:
+                        fragment = new RoomsAvailable();
+                        break;
+                    case R.id.nav_booked_rooms:
+                        fragment = new BookedRooms();
+                        break;
+                    case R.id.nav_edit_profile:
+                        fragment = new EditProfile();
+                        break;
+                    case R.id.nav_logout:
+                        firebaseAuth.signOut();
+                        Intent signoutIntent = new Intent(StaffDashboardActivity.this, UserLoginActivity.class);
+                        signoutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(signoutIntent);
+                        break;
+                }
+
+                if (fragment != null) {
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.content_frame, fragment);
+                    ft.commit();
+                }
+
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        navigationView.setCheckedItem(R.id.nav_rooms_available);
+
+
     }
+
+
+
+
 }
+
