50944dce21c26e73f070deaf172cfe34c21a6cef
tagorekommineni9 <tagorek7@kluniversity.in>
2020-06-04 08:46:04 -0400

    Added spinner for Viewholder, added query to list all rooms

--- app/src/main/java/com/example/roombookingsystem/activities/admin/AdminAllRooms.java
+++ app/src/main/java/com/example/roombookingsystem/activities/admin/AdminAllRooms.java
@@ -1,19 +1,46 @@
 package com.example.roombookingsystem.activities.admin;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.roombookingsystem.R;
+import com.example.roombookingsystem.activities.admin.rooms.Rooms;
+import com.example.roombookingsystem.activities.admin.rooms.RoomsAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AdminAllRooms extends Fragment {
+
+    public static final String ROOM_ID = "roomno";
+    public static final String ROOM_CAPACITY = "roomcapacity";
+    public static final String ROOM_SOFTWARE = "software";
+    public static final String ROOM_HARDWARE = "hardware";
+    public static final String ROOM_IS_AVAILABLE = "available";
+    private DatabaseReference mRoomsDatabase;
+    private RecyclerView mRoomRecyclerView;
+    private RoomsAdapter mRoomItemAdapter;
+    private RecyclerView.LayoutManager mRoomLayoutManager;
+    String roomID, roomCapacity, roomSoftware, roomHardware, available;
+    boolean is_Available;
 
     public AdminAllRooms() {
         // Required empty public constructor
@@ -25,5 +52,97 @@ public class AdminAllRooms extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_admin_all_rooms, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mRoomRecyclerView = getView().findViewById(R.id.recycler_admin_all_rooms);
+        mRoomRecyclerView.setNestedScrollingEnabled(false);
+        mRoomRecyclerView.setHasFixedSize(true);
+        mRoomLayoutManager = new LinearLayoutManager(getActivity());
+        mRoomRecyclerView.setLayoutManager(mRoomLayoutManager);
+        mRoomItemAdapter = new RoomsAdapter(getRoomListing(), getActivity());
+        mRoomRecyclerView.setAdapter(mRoomItemAdapter);
+
+        setRoomData();
+
+        if(getRoomListing() != null)
+        {
+            mRoomItemAdapter.setOnItemClickListener(new RoomsAdapter.OnItemClickListener() {
+                @Override
+                public void onItemClick(int position) {
+                    Rooms room = getRoomListing().get(position);
+                    Intent intent = new Intent(getActivity(), AdminModifyRoom.class);
+                    intent.putExtra(ROOM_ID, room.getRoomno());
+                    intent.putExtra(ROOM_CAPACITY, room.getRoomcapacity());
+                    intent.putExtra(ROOM_HARDWARE, room.getHardware());
+                    intent.putExtra(ROOM_SOFTWARE, room.getSoftware());
+                    intent.putExtra(ROOM_IS_AVAILABLE, String.valueOf(room.isAvailable()));
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+    private void setRoomData() {
+        mRoomsDatabase = FirebaseDatabase.getInstance().getReference().child("rooms");
+
+        mRoomsDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.exists()){
+
+                    for(DataSnapshot roomList : dataSnapshot.getChildren()){
+
+                        getRoomListDbInformation(roomList.getKey());
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getRoomListDbInformation(String key) {
+
+        DatabaseReference RoomKeyRef = mRoomsDatabase.child(key);
+
+        RoomKeyRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    roomID = dataSnapshot.child("roomno").getValue().toString();
+                    roomCapacity = dataSnapshot.child("roomcapacity").getValue().toString();
+                    roomSoftware = dataSnapshot.child("software").getValue().toString();
+                    roomHardware = dataSnapshot.child("hardware").getValue().toString();
+                    available = dataSnapshot.child("available").getValue().toString();
+                    if(available.equals("true"))
+                        is_Available = true;
+                    else
+                        is_Available = false;
+                    Rooms roomObj = new Rooms(roomID,roomCapacity, roomHardware, roomSoftware, is_Available);
+                    roomListingResult.add(roomObj);
+                    mRoomItemAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private ArrayList<Rooms> roomListingResult= new ArrayList<Rooms>();
+
+    private List<Rooms> getRoomListing() {
+        return roomListingResult;
     }
 }
--- app/src/main/java/com/example/roombookingsystem/activities/admin/rooms/RoomsViewHolder.java
+++ app/src/main/java/com/example/roombookingsystem/activities/admin/rooms/RoomsViewHolder.java
@@ -1,6 +1,7 @@
 package com.example.roombookingsystem.activities.admin.rooms;
 
 import android.view.View;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,15 +13,30 @@ public class RoomsViewHolder extends RecyclerView.ViewHolder {
 
 
     public TextView mRoomNo, mRoomCapacity, mHardware, mSoftware;
+    Spinner mAvailable;
 
 
-    public RoomsViewHolder(@NonNull View itemView) {
+    public RoomsViewHolder(@NonNull View itemView, final RoomsAdapter.OnItemClickListener listener) {
         super(itemView);
 
         mRoomNo = itemView.findViewById(R.id.tv_room_no);
         mRoomCapacity = itemView.findViewById(R.id.tv_room_capacity);
         mHardware = itemView.findViewById(R.id.tv_hardware_equipment);
         mSoftware = itemView.findViewById(R.id.tv_software_equipment);
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(listener != null)
+                {
+                    int position = getAdapterPosition();
+                    if(position!=RecyclerView.NO_POSITION)
+                    {
+                        listener.onItemClick(position);
+                    }
+                }
+            }
+        });
     }
 
 
