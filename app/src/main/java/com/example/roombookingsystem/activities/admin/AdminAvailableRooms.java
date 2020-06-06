a79e7973ee263209d4bcc73580b72caeab8843e0
tagorekommineni9 <tagorek7@kluniversity.in>
2020-06-05 23:37:46 -0400

    Get list of available rooms from database

--- app/src/main/java/com/example/roombookingsystem/activities/admin/AdminAvailableRooms.java
+++ app/src/main/java/com/example/roombookingsystem/activities/admin/AdminAvailableRooms.java
@@ -2,18 +2,38 @@ package com.example.roombookingsystem.activities.admin;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.roombookingsystem.R;
+import com.example.roombookingsystem.activities.admin.rooms.Rooms;
+import com.example.roombookingsystem.activities.admin.rooms.RoomsAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AdminAvailableRooms extends Fragment {
+
+    private DatabaseReference mRoomsDatabase;
+    private RecyclerView mRoomRecyclerView;
+    private RoomsAdapter mRoomItemAdapter;
+    private RecyclerView.LayoutManager mRoomLayoutManager;
+    String roomID, roomCapacity, roomSoftware, roomHardware, available;
 
     public AdminAvailableRooms() {
         // Required empty public constructor
@@ -25,5 +45,77 @@ public class AdminAvailableRooms extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_admin_available_rooms, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mRoomRecyclerView = getView().findViewById(R.id.recycler_admin_available_rooms);
+        mRoomRecyclerView.setNestedScrollingEnabled(false);
+        mRoomRecyclerView.setHasFixedSize(true);
+        mRoomLayoutManager = new LinearLayoutManager(getActivity());
+        mRoomRecyclerView.setLayoutManager(mRoomLayoutManager);
+        mRoomItemAdapter = new RoomsAdapter(getRoomListing(), getActivity());
+        mRoomRecyclerView.setAdapter(mRoomItemAdapter);
+
+        setRoomData();
+    }
+
+    private void setRoomData() {
+        mRoomsDatabase = FirebaseDatabase.getInstance().getReference().child("rooms");
+
+        mRoomsDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.exists()){
+
+                    for(DataSnapshot roomList : dataSnapshot.getChildren()){
+
+                        getRoomListDbInformation(roomList.getKey());
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void getRoomListDbInformation(String key) {
+
+        DatabaseReference RoomKeyRef = mRoomsDatabase.child(key);
+
+        RoomKeyRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                available = dataSnapshot.child("available").getValue().toString();
+                if(dataSnapshot.exists() && available.equals("true")){
+                    roomID = dataSnapshot.child("roomno").getValue().toString();
+                    roomCapacity = dataSnapshot.child("roomcapacity").getValue().toString();
+                    roomSoftware = dataSnapshot.child("software").getValue().toString();
+                    roomHardware = dataSnapshot.child("hardware").getValue().toString();
+
+                    Rooms roomObj = new Rooms(roomID,roomCapacity,roomSoftware,roomHardware);
+                    roomListingResult.add(roomObj);
+                    mRoomItemAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private ArrayList<Rooms> roomListingResult= new ArrayList<Rooms>();
+
+    private List<Rooms> getRoomListing() {
+        return roomListingResult;
     }
 }
