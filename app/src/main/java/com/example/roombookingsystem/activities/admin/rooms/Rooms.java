cba01a989c1b3755bfb173040ea8c769a41b1243
Tanyachawla01 <tanyachawla2227@gmail.com>
2020-06-19 13:46:44 -0400

    Added requested Eqipment and booking purpose in Room class

--- app/src/main/java/com/example/roombookingsystem/activities/admin/rooms/Rooms.java
+++ app/src/main/java/com/example/roombookingsystem/activities/admin/rooms/Rooms.java
@@ -190,5 +190,7 @@ public class Rooms {
         this.roomimage = roomimage;
         this.staffname = staffname;
         this.staffId = staffId;
+        this.requestedEquipment = requestedEquipment;
+        this.bookingPurpose = bookingPurpose;
     }
 }
