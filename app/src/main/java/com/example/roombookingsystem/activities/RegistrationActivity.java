3695c02c3199da6db5d012bf3953bb4ced7e2bfa
Tanyachawla01 <tanyachawla2227@gmail.com>
2020-06-21 14:22:58 -0400

    Added email format checking for emails in registration

--- app/src/main/java/com/example/roombookingsystem/activities/RegistrationActivity.java
+++ app/src/main/java/com/example/roombookingsystem/activities/RegistrationActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,9 +24,20 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^" +
+            "(?=.*[0-9])" +         //at least 1 digit
+            "(?=.*[a-z])" +         //at least 1 lower case letter
+            "(?=.*[A-Z])" +         //at least 1 upper case letter
+            "(?=.*[a-zA-Z])" +      //any letter
+            "(?=.*[@#$%^&+=])" +    //at least 1 special character
+            "(?=\\S+$)" +           //no white spaces
+            ".{4,10}" +               //at least 4 characters and maximum of 10 characters
+            "$");
 
     EditText mName, mPhone, mEmail, mPassword, mConfirmPassword;
     Button mRegister;
@@ -74,8 +86,17 @@ public class RegistrationActivity extends AppCompatActivity {
                     mPhone.setError("Enter your Phone number");
                 } else if (TextUtils.isEmpty(mEmail.getText().toString())) {
                     mEmail.setError("Enter your Email");
+
+                    //Email pattern check
+                }  else if (!Patterns.EMAIL_ADDRESS.matcher(mEmail.getText().toString()).matches()) {
+                    mEmail.setError("Please enter a valid email address");
+
                 } else if (TextUtils.isEmpty(mPassword.getText().toString())) {
                     mPassword.setError("Enter your Password");
+
+                    //password pattern check
+                } else if (!PASSWORD_PATTERN.matcher(mPassword.getText().toString()).matches()) {
+                    mPassword.setError("Password too weak! please include atleast 1 number, 1 uppercase letter, 1 lowercase letter, 1 special character(@#$%^&+=)");
                 } else if (TextUtils.isEmpty(mConfirmPassword.getText().toString())) {
                     mConfirmPassword.setError("Please confirm your password");
                 } else if (!(mConfirmPassword.getText().toString().equals(mPassword.getText().toString()))){
@@ -93,24 +114,37 @@ public class RegistrationActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if(task.isSuccessful()){
 
-                                String staffId = firebaseAuth.getCurrentUser().getUid();
-                                DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("users").child(staffId);
+                                firebaseAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if(task.isSuccessful()) {
+                                            String staffId = firebaseAuth.getCurrentUser().getUid();
+                                            DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("users").child(staffId);
 
-                                Map staffInfo = new HashMap<>();
+                                            Map staffInfo = new HashMap<>();
 
-                                staffInfo.put("name", name);
-                                staffInfo.put("email", email);
-                                staffInfo.put("phone", phone);
-                                staffInfo.put("type", "staff");
+                                            staffInfo.put("name", name);
+                                            staffInfo.put("email", email);
+                                            staffInfo.put("phone", phone);
+                                            staffInfo.put("type", "staff");
 
-                                currentUserDb.updateChildren(staffInfo);
+                                            currentUserDb.updateChildren(staffInfo);
 
-                                Toast.makeText(RegistrationActivity.this, "Staff ID is created successfully.", Toast.LENGTH_SHORT).show();
-                                clearFields();
+                                            Toast.makeText(RegistrationActivity.this, "Registered successfully. Please check your email for Verification.", Toast.LENGTH_SHORT).show();
+                                            clearFields();
+                                        }
+                                        else{
+                                            Toast.makeText(RegistrationActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
 
-                                Intent staffIntent = new Intent(RegistrationActivity.this, StaffDashboardActivity.class);
+                                        }
+                                    }
+                                });
+
+
+
+                               /* Intent staffIntent = new Intent(RegistrationActivity.this, StaffDashboardActivity.class);
                                 staffIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_NEW_TASK);
-                                startActivity(staffIntent);
+                                startActivity(staffIntent); */
 
 
 
