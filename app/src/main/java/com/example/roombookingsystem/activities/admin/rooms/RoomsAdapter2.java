9befc3ddb17e803bcf1d06e4ac6afc78cf80eccd
samamahe <samamaheshreddy007@gmail.com>
2020-06-22 19:16:36 -0400

    Created New room Adapters for Allrooms and Available Rooms

---
+++ app/src/main/java/com/example/roombookingsystem/activities/admin/rooms/RoomsAdapter2.java
@@ -0,0 +1,109 @@
+package com.example.roombookingsystem.activities.admin.rooms;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.roombookingsystem.R;
+
+import java.util.List;
+
+public class RoomsAdapter2 extends  RecyclerView.Adapter<RoomsViewHolder2>{
+
+    private List<Rooms> RoomList;
+    Context context;
+    private OnItemClickListener mListener;
+    //private DatabaseReference mBookingsDatabase;
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener)
+    {
+        mListener = listener;
+    }
+
+    public RoomsAdapter2(List<Rooms> roomList, Context context) {
+        RoomList = roomList;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public RoomsViewHolder2 onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View roomLayoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_available_rooms, null, false);
+        RecyclerView.LayoutParams roomLayoutParams = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        roomLayoutView.setLayoutParams(roomLayoutParams);
+        return new RoomsViewHolder2(roomLayoutView, mListener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final RoomsViewHolder2 holder, final int position) {
+
+        holder.mSoftware.setText(RoomList.get(position).getSoftware());
+        holder.mHardware.setText(RoomList.get(position).getHardware());
+        holder.mRoomCapacity.setText(RoomList.get(position).getRoomcapacity());
+        holder.mRoomNo.setText(RoomList.get(position).getRoomno());
+        holder.mBlock.setText(RoomList.get(position).getBlock());
+        holder.mFloor.setText(RoomList.get(position).getFloor());
+
+        if(!RoomList.get(position).getRoomimage().equals("default")){
+            Glide.with(context).load(RoomList.get(position).getRoomimage()).into(holder.mRoomImage);
+        }
+
+        /*mBookingsDatabase = FirebaseDatabase.getInstance().getReference().child("bookings");
+        mBookingsDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(RoomList.get(position).getRoomno()).exists()){
+                    holder.mTableLayout.setVisibility(View.GONE);
+                    holder.mStaffName.setText(RoomList.get(position).getStaffname());
+                    holder.mRequestedEquipmentLayout.setVisibility(View.GONE);
+                    holder.mRequestedEquipment.setText(RoomList.get(position).getRequestedEquipment());
+                    holder.mBookingPurposeLayout.setVisibility(View.GONE);
+                    holder.mBookingPurpose.setText(RoomList.get(position).getBookingPurpose());
+                    holder.mBookingDate.setText(RoomList.get(position).getBookingDate());
+                    holder.mBookingDateLayout.setVisibility(View.GONE);
+                    holder.mStartTime.setText(RoomList.get(position).getStartTime());
+                    holder.mStartTimeLayout.setVisibility(View.GONE);
+                    holder.mEndTime.setText(RoomList.get(position).getEndTime());
+                    holder.mEndTimeLayout.setVisibility(View.GONE);
+                }
+                else {
+                    holder.mTableLayout.setVisibility(View.GONE);
+                    holder.mRequestedEquipmentLayout.setVisibility(View.GONE);
+                    holder.mBookingPurposeLayout.setVisibility(View.GONE);
+                    holder.mBookingDateLayout.setVisibility(View.GONE);
+                    holder.mStartTimeLayout.setVisibility(View.GONE);
+                    holder.mEndTimeLayout.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });*/
+
+        /*if(!RoomList.get(position).getStaffname()){
+
+            holder.mStaffName.setText(RoomList.get(position).getStaffname());
+        }
+        else {
+            holder.mTableLayout.setVisibility(View.GONE);
+        }*/
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return RoomList.size();
+    }
+}
---
+++ app/src/main/java/com/example/roombookingsystem/activities/admin/rooms/RoomsViewHolder2.java
@@ -0,0 +1,64 @@
+package com.example.roombookingsystem.activities.admin.rooms;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.roombookingsystem.R;
+
+public class RoomsViewHolder2 extends RecyclerView.ViewHolder {
+
+
+    public TextView mRoomNo, mRoomCapacity, mHardware, mSoftware, mBlock, mFloor, mStaffName, mRequestedEquipment, mBookingPurpose, mBookingDate, mStartTime, mEndTime;
+    Spinner mAvailable;
+    ImageView mRoomImage;
+    TableRow mTableLayout, mRequestedEquipmentLayout, mBookingPurposeLayout, mBookingDateLayout, mStartTimeLayout, mEndTimeLayout;
+
+
+    public RoomsViewHolder2(@NonNull View itemView, final RoomsAdapter2.OnItemClickListener listener) {
+        super(itemView);
+
+        mRoomNo = itemView.findViewById(R.id.tv_room_no);
+        mRoomCapacity = itemView.findViewById(R.id.tv_room_capacity);
+        mHardware = itemView.findViewById(R.id.tv_hardware_equipment);
+        mSoftware = itemView.findViewById(R.id.tv_software_equipment);
+        mBlock = itemView.findViewById(R.id.tv_block);
+        mFloor = itemView.findViewById(R.id.tv_floor);
+        mRoomImage = itemView.findViewById(R.id.image_btn);
+        /*mStaffName = itemView.findViewById(R.id.tv_staff_name);
+        mRequestedEquipment = itemView.findViewById(R.id.tv_requested_equipment);
+        mBookingPurpose = itemView.findViewById(R.id.tv_booking_purpose);
+        mTableLayout = itemView.findViewById(R.id.staff_layout);
+        mRequestedEquipmentLayout = itemView.findViewById(R.id.requestedEquipment_layout);
+        mBookingPurposeLayout = itemView.findViewById(R.id.bookingPurpose_layout);
+        mBookingDate = itemView.findViewById(R.id.tv_bookingDate);
+        mStartTime = itemView.findViewById(R.id.tv_start_time);
+        mEndTime = itemView.findViewById(R.id.tv_end_time);
+        mBookingDateLayout = itemView.findViewById(R.id.bookingDate_layout);
+        mStartTimeLayout = itemView.findViewById(R.id.startTime_layout);
+        mEndTimeLayout = itemView.findViewById(R.id.endTime_layout);*/
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(listener != null)
+                {
+                    int position = getAdapterPosition();
+                    if(position!= RecyclerView.NO_POSITION)
+                    {
+                        listener.onItemClick(position);
+                    }
+                }
+            }
+        });
+    }
+
+
+
+
+}
